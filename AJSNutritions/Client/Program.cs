a8aa6e06e88150e9f11140e28cfb8e95998d67cd
ADAM IBRAHIM HOUGH 2100768G <2100768G@student.tp.edu.sg>
2024-02-03 23:44:50 +0800

    Updated with shon code

--- AJSNutritions/Client/Program.cs
+++ AJSNutritions/Client/Program.cs
@@ -1,4 +1,5 @@
 using AJSNutritions.Client;
+using AJSNutritions.Client.Services;
 using AJSNutritions.Client.Services.DishService;
 using AJSNutritions.Client.Services.FoodLogService;
 using AJSNutritions.Client.Services.StaffService;
@@ -6,13 +7,19 @@ using AJSNutritions.Client.Services.UserService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("AJSNutritions.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("AJSNutritions.ServerAPI", (sp, 
+client) => {
+    client.BaseAddress = new
+Uri(builder.HostEnvironment.BaseAddress);
+    client.EnableIntercept(sp);
+        })
+.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("AJSNutritions.ServerAPI"));
@@ -20,6 +27,8 @@ builder.Services.AddScoped<IDishService, DishService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IStaffService, StaffService>();
 builder.Services.AddScoped<IFoodLogService, FoodLogService>();
+builder.Services.AddHttpClientInterceptor();
+builder.Services.AddScoped<HttpInterceptorService>();
 builder.Services.AddApiAuthorization();
 
 
